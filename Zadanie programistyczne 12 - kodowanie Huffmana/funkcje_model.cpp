ba69c6d8bdcc63089ea82ef72073aabd61334e88
dawibie029 <dawibie029@student.polsl.pl>
2020-11-01 15:26:36 +0100

    Zmiana parametrów dotychczasowych funkcji - od teraz zamiast wektorów typu string, funkcje przyjmują mapę zawierającą pary znak-kod

--- Zadanie programistyczne 12 - kodowanie Huffmana/funkcje_model.cpp
+++ Zadanie programistyczne 12 - kodowanie Huffmana/funkcje_model.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <map>
 #include "funkcje_model.h"
 
 /**
@@ -12,7 +13,7 @@
 	* @param dane - wektor zawieraj¹cy tablicê kodów, oraz s³owa kodowe (na ostatniej pozycji)
 	* @return - zwraca wartoœæ true, je¿eli uda siê otworzyæ plik. W przeciwnym wypadku wartosc false
 */
-bool read(const std::string& adres, std::vector<std::string>& dane) {
+/*bool read(const std::string& adres, std::vector<std::string>& dane) {
 	std::ifstream plikwe(adres);
 	if (!plikwe.is_open())
 		return false;
@@ -22,22 +23,65 @@ bool read(const std::string& adres, std::vector<std::string>& dane) {
 		dane.push_back(napis);
 	}
 	return true;
+}*/
+/**
+	* Funkcja wczytuj¹ca zawartoœæ pliku tekstowego zapisanego w formacie: s³owa_kodowe;znak,wartosc,znak,wartosc...
+	* Znaki i odpowiadaj¹ce im kody s¹ zapisywane w zmiennej mapa_wartosci, a zakodowane dane w zmiennej slowa_kodowe
+	* Parametry przekazywane przez referencjê
+	* @param adres - adres pliku wejœciowego
+	* @param mapa_wartosci - mapa, w której zapisane zostan¹ znaki i odpowiadaj¹ce im kody
+	* @param slowa_kodowe - zmienna, w której zapisana zostanie zakodowana wiadomoœæ
+	* @return - zwraca wartoœæ true, je¿eli uda siê otworzyæ plik. W przeciwnym wypadku wartosc false
+*/
+bool read_coded(const std::string& adres, std::map<char, std::string>& mapa_wartosci, std::string& slowa_kodowe){
+	std::ifstream plikwe(adres);
+	if (!plikwe.is_open())
+		return false;
+	getline(plikwe, slowa_kodowe, ';');	//*	odczytuje zakodowane slowa i zapisuje w zmiennej
+	while (!plikwe.eof()) {		
+		char znak;
+		std::string wartosc;
+		plikwe.get(znak);	//*Pobiera znak
+		plikwe.get();	//	Pomija symbol oddzielaj¹cy znaki od definicji (dla pliku wejœciowego dowolny, domyœlnie (oraz w pliku wyjœciowym) u¿ywany jest przecinek)
+		getline(plikwe, wartosc, ',');	//	Wczytuje wartoœæ znaku wczytanego w linijce wy¿ej. Oddzielaj¹c kod od nastêpnego znaku konieczne jest ju¿ u¿ycie przecinka.
+		mapa_wartosci[znak] = wartosc;
+	}
+	return true;
 }
+
 /**
 	* Funkcja zapisuj¹ca podany tekst w pliku tekstowym o wskazanym adresie
 	* Parametry przekazywane przez referencje
 	* @param adres - adres pliku wyjœciowego
-	* @param tekst - wektor zawieraj¹cy dane, które funckja zapisze w pliku wyjœciowym
+	* @param mapa_wartosci - mapa zawieraj¹ca pary znak-wartoœæ
+	* @param slowa_kodowe - zmienna zawieraj¹ca zakodowan¹ wiadomoœæ
 	* @return zwraca wartoœæ true, jeœli funkcja zadzia³a prawid³owo lub wartoœæ false w razie niepowodzenia
 */
-bool save(const std::string& adres, const std::vector<std::string>& dane) {
+bool save_coded(const std::string& adres, const std::map<char, std::string>& mapa_wartosci, const std::string& slowa_kodowe) {
+	std::ofstream plikwy(adres);
+	if (!plikwy.is_open())
+		return false;
+	plikwy << slowa_kodowe << ";";
+	int i = 0;
+	for (auto a : mapa_wartosci) {
+		if (i++ != 0) {
+			plikwy << ',' << a.first << ',' << a.second;	
+		}
+		else {
+			plikwy << a.first << ',' << a.second;
+		}
+	}
+	return true;
+}
+
+/*bool save(const std::string& adres, const std::vector<std::string>& dane) {
 	std::ofstream plikwy(adres);
 	if (!plikwy.is_open())
 		return false;
 	for (std::string a : dane)	//	Pêtla maj¹ca na celu zapisanie do pliku tekstowego wartoœci wektora dane rozdzielonych œrednikami
 		plikwy << a << ';';	
 	return true;
-}
+}*/
 /**
 	* Funkcja konwertuj¹ca uzyskany wektor z danymi na dwie tablice - znaków i wartoœci oraz na zmienn¹ zawieraj¹c¹ s³owa kodowe
 	* Parametry przekazywane przez referencje
@@ -46,13 +90,13 @@ bool save(const std::string& adres, const std::vector<std::string>& dane) {
 	* @param wartoœci - tablica wartoœci przypisanych do odpowiadaj¹cych im znaków
 	* @param kod - ci¹g s³ów kodowych
 */
-void convert(const std::vector<std::string>& dane, std::vector<std::string>& znaki, std::vector<std::string>& wartosci, std::string& kod) {
+/*void convert(const std::vector<std::string>& dane, std::vector<std::string>& znaki, std::vector<std::string>& wartosci, std::string& kod) {
 	for (std::size_t i = 0; i < dane.size() - 1; i += 2) {	//	Pêtla przepisuj¹ca wartoœci z wektora dane do dwóch wektorów w sposób wynikaj¹cy ze sposobu
 		znaki.push_back(dane[i]);							//	zapisu w tym wektorze - na przemian znak i odpowiadaj¹cy mu kod binarny (zawsze liczba parzysta).
 		wartosci.push_back(dane[i + 1]);					//	Po wykonaniu pêtli zawsze zostaje jeden element - jest nim zakodowane zdanie
 	}
 	kod = dane.back();
-}
+}*/
 /**
 	* Funkcja konwertuj¹ca wektory znaki i dane oraz zakodowany tekst na jeden wektor w formacie gotowym do zapisu jako plik tekstowy
 	* Parametry przekazywane przez referencje
@@ -61,11 +105,11 @@ void convert(const std::vector<std::string>& dane, std::vector<std::string>& zna
 	* @param wartoœci - tablica wartoœci przypisanych do odpowiadaj¹cych im znaków
 	* @param kod - ci¹g s³ów kodowych
 */
-void convert_back(std::vector<std::string>& dane, const std::vector<std::string>& znaki, const std::vector<std::string>& wartosci, const std::string& kod) {
+/*void convert_back(std::vector<std::string>& dane, const std::vector<std::string>& znaki, const std::vector<std::string>& wartosci, const std::string& kod) {
 	dane.clear();	// dla upewnienia siê, ¿e wektor dane jest pusty.
 	for (std::size_t i = 0; i < wartosci.size(); i++) {
 		dane.push_back(znaki[i]);
 		dane.push_back(znaki[i + 1]);
 	}
 	dane.push_back(kod);
-}
+}*/
--- Zadanie programistyczne 12 - kodowanie Huffmana/main.cpp
+++ Zadanie programistyczne 12 - kodowanie Huffmana/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include "funkcje_model.h"
 
 int main(int argc, char* argv[]) {
-	std::vector<std::string> vec;
+	/*std::vector<std::string> vec;
 	read("C:/Users/Dawid/Desktop/test1.txt", vec);
 	for (auto a : vec) {
 		std::cout << a << std::endl;
@@ -17,4 +18,13 @@ int main(int argc, char* argv[]) {
 		std::cout << znaki[i] << ": " << wartosci[i] << std::endl;
 	}
 	std::cout << kod << std::endl;
+	std::vector<std::map<char, std::string>> mapa;*/
+	std::map<char, std::string> mapa;
+	std::string tekst;
+	read("C:/Users/Dawid/Desktop/test1.txt", mapa, tekst);
+	for (auto a : mapa) {
+		std::cout << a.first << " - " << a.second << std::endl;
+	}
+	std::cout << tekst << std::endl;
+	save("C:/Users/Dawid/Desktop/test2.txt", mapa, tekst);
 }
